4084c73fbcfea9d4844171da65f25abaef331c64
Joe Thornber <ejt@redhat.com>
2016-11-22 07:32:26 -0500

    Add unit test for parsing of /proc/devices file

--- src/device_mapper/utils.rs
+++ src/device_mapper/utils.rs
@@ -30,9 +30,90 @@ fn parse_majors(reader: &mut std::io::BufRead) -> Option<Vec<u32>> {
     Some(majors)
 }
 
-// FIXME: write test
+#[cfg(test)]
+mod tests {
+    fn __parse_test(t: (&Option<Vec<u32>>, &[u8])) {
+        let mut ptr = &t.1[..];
+        assert_eq!(super::parse_majors(&mut ptr), *t.0);
+    }
 
-/// Major numbers used by DM.
+    #[test]
+    fn test_parse_majors() {
+        let empty = Some(vec![]);
+
+        __parse_test((&empty, b""));
+        __parse_test((&empty, b"flippity"));
+
+        __parse_test((&empty,
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty"#));
+
+        __parse_test((&empty,
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty
+128 device-mapper"#));
+
+        __parse_test((&Some(vec![128u32]),
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty
+
+Block devices:
+128 device-mapper"#));
+
+        __parse_test((&Some(vec![28u32]),
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty
+
+Block devices:
+ 28 device-mapper"#));
+
+        __parse_test((&Some(vec![28u32]),
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty
+
+Block devices:
+ 28 device-mapper"#));
+
+        __parse_test((&Some(vec![28u32, 128]),
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty
+
+Block devices:
+  1 foo
+ 28 device-mapper
+111 bar
+128 device-mapper
+199 hux"#));
+
+        __parse_test((&None,
+                      br#"Character devices:
+  1 mem
+  4 /dev/vc/0
+  4 tty
+
+Block devices:
+  1 foo
+ 28 device-mapper
+111 bar
+1o8 device-mapper
+199 hux"#));
+    }
+}
+
+/// Read and parse /proce/devices to ascertain the major numbers used
+/// by DM.
 pub fn dm_majors() -> io::Result<Vec<u32>> {
     let f = try!(File::open("/proc/devices"));
     let mut reader = io::BufReader::new(f);
